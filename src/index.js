b33d08d87f6c88af58652760fed939e1c89555b0
Khairul Hidayat <contact@khairul.my.id>
2020-01-15 22:10:47 +0700

    Added eslint config

--- src/index.js
+++ src/index.js
@@ -3,38 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'typeface-roboto';
 import {
-    Grid,
-    Typography as Text,
-    makeStyles
-} from '@material-ui/core'
-
-const App = () => {
-    const styles = useStyles();
-
-    return (
-        <div className={styles.root}>
-            <Grid container>
-                <Grid item xs={6} className={styles.test}>
-                    <Text variant='h2' gutterBottom>Hello world!</Text>
-                </Grid>
-                <Grid item xs={6} className={styles.test1}>
-                    <Text color='primary'>Test</Text>
-                </Grid>
-            </Grid>
-        </div>
-    );
-}
+  Grid,
+  Typography as Text,
+  makeStyles,
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
-    root: {
-        height: '100vh',
-    },
-    test: {
-        background: 'red'
-    },
-    test1: {
-        background: 'blue'
-    }
+  root: {
+    height: '100vh',
+  },
+  test: {
+    background: 'red',
+  },
+  test1: {
+    background: 'blue',
+  },
 });
+
+const App = () => {
+  const styles = useStyles();
+
+  return (
+    <Grid container className={styles.root}>
+      <Grid item xs={6} className={styles.test}>
+        <Text variant="h2" gutterBottom>Hello world!</Text>
+      </Grid>
+      <Grid item xs={6} className={styles.test1}>
+        <Text color="primary">Test</Text>
+      </Grid>
+    </Grid>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
