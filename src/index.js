05de48858561a61a2ceb043ee77c23fedb0a7c67
Khairul Hidayat <contact@khairul.my.id>
2020-01-15 21:22:06 +0700

    Added material ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
+import {
+    Typography as Text,
+    makeStyles
+} from '@material-ui/core'
 
 const App = () => {
-    return <p>Hello world!</p>;
+    const styles = useStyles();
+
+    return (
+        <div>
+            <Text variant='h2' className={styles.test}>Hello world!</Text>
+            <Text color='primary'>Test</Text>
+        </div>
+    );
 }
+
+const useStyles = makeStyles({
+    test: {
+        marginBottom: 10
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
