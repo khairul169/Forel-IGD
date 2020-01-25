d25a31e8a6a9403d09676a175816c04229a0d204
Khairul Hidayat <contact@khairul.my.id>
2020-01-25 15:27:21 +0700

    h

--- src/Login/Daftar.js
+++ src/Login/Daftar.js
@@ -5,9 +5,11 @@ import {
   Typography,
   Button,
   Link,
+  Select,
+  MenuItem,
 } from '@material-ui/core';
 import {
-  PermContactCalendar, AccountCircle, LockOutlined, Email,
+  PermContactCalendar, AccountCircle, LockOutlined, Email, HowToReg,
 } from '@material-ui/icons';
 import { Link as RouterLink } from 'react-router-dom';
 import lockIcon from '../Images/lock.png';
@@ -52,6 +54,18 @@ const Daftar = () => (
           fullWidth
           label="Email"
         />
+      </Grid>
+    </Grid>
+
+    <Grid container alignItems="center" style={{ marginTop: 16 }}>
+      <Grid item>
+        <HowToReg style={{ marginTop: 20, fontSize: 24, marginRight: 16 }} />
+      </Grid>
+      <Grid item xs>
+        <Select value="1" labelId="tipe-akun" fullWidth>
+          <MenuItem value="1">Pendaftaran</MenuItem>
+          <MenuItem value="2">Dokter</MenuItem>
+        </Select>
       </Grid>
     </Grid>
 
