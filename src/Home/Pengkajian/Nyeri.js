69610969e985f39071f96452205b895b56791b33
Khairul Hidayat <contact@khairul.my.id>
2020-01-29 01:50:08 +0700

    Updated medic obs form

--- src/Home/Pengkajian/Nyeri.js
+++ src/Home/Pengkajian/Nyeri.js
@@ -13,12 +13,44 @@ export const defaultValues = {
   upayaNyeri: '',
   gambaranNyeri: '0',
   gambaranNyeriLain: '',
+  penyebaranNyeri: '0',
+  menyebarKe: '',
+  skalaNyeri: '0',
+  nyeriNips: '',
+  nyeriBps: '',
+  nyeriNrs: '',
+  nyeriWongBaker: '',
+  nyeriFlacc: '',
 };
 
 const Nyeri = ({ styles, form }) => {
   const gambaranNyeri = ['Ditusuk-tusuk Benda Tajam', 'Rasa Terbakar/Panas', 'Tertekan Benda Berat', {
     input: 'gambaranNyeriLain',
     label: 'Lain-lain',
+  }];
+  const penyebaranNyeri = ['Di Satu Lokasi Tertentu', {
+    input: 'menyebarKe',
+    label: 'Menyebar Ke',
+  }];
+  const skalaNyeri = [{
+    input: 'nyeriNips',
+    label: 'NIPS',
+  },
+  {
+    input: 'nyeriBps',
+    label: 'BPS',
+  },
+  {
+    input: 'nyeriNrs',
+    label: 'NRS',
+  },
+  {
+    input: 'nyeriWongBaker',
+    label: 'Wong Baker Faces',
+  },
+  {
+    input: 'nyeriFlacc',
+    label: 'FLACC',
   }];
 
   return (
@@ -41,6 +73,14 @@ const Nyeri = ({ styles, form }) => {
 
       <UserInput title="Gambaran Nyeri Yang Dirasakan">
         <FormOption name="gambaranNyeri" form={form} items={gambaranNyeri} cols={3} />
+      </UserInput>
+
+      <UserInput title="Penyebaran Nyeri">
+        <FormOption name="penyebaranNyeri" form={form} items={penyebaranNyeri} cols={2} />
+      </UserInput>
+
+      <UserInput title="Skala Nyeri">
+        <FormOption name="skalaNyeri" form={form} items={skalaNyeri} cols={2} />
       </UserInput>
     </Panel>
   );
