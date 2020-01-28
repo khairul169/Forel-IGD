6698288eedc19948453001d4be3f9e0c6c35d060
Khairul Hidayat <contact@khairul.my.id>
2020-01-28 19:23:54 +0700

    Fix logout

--- src/Home/index.js
+++ src/Home/index.js
@@ -90,6 +90,7 @@ const Home = ({ userData, setAuthToken, setUserData }) => {
       setUserData(user.result);
     } catch (error) {
       sessionStorage.clear();
+      setAuthToken(null);
       setRedirectLogin(true);
     }
   };
